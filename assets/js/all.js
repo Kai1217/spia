1c737e0fb8da0c0b33419d69d722523d31297102
Kai Matolat <kaimatolat@hotmail.com>
2018-11-18 21:54:00 +1000

    Added all of the people images. Included examples of Machine Learning with TensorFlow.JS. Under the Apache-2.0 License from https://github.com/tensorflow

--- assets/js/all.js
+++ assets/js/all.js
@@ -77,6 +77,32 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 } 
+/* Third */
+// Get the modal
+var modal3 = document.getElementById('myModal3');
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close3")[0];
+
+// When the user clicks on the button, open the modal
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+    modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+}
 /* Second */
 // Get the modal
 var modal2 = document.getElementById('myModal2');
@@ -102,7 +128,7 @@ window.onclick = function(event) {
     if (event.target == modal2) {
         modal2.style.display = "none";
     }
-} 
+}  
 function goBack() {
     window.history.back();
 }
